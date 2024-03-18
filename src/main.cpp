254601522a90573f6eee2c3ad2f00a82883552d6
luiscab19 <luis19alvarez05@gmail.com>
2024-03-18 18:04:54 -0400

    feat: prototipo de las funciones a utilizar

--- src/main.cpp
+++ src/main.cpp
@@ -35,3 +35,33 @@ struct Car {
 	int sold_for;
 	int bought_for;
 };
+
+#pragma region Prototipos
+void ContadorCliente();
+void ContadorAutos();
+void MenuPrincipal();
+
+void MenuAgregar();
+void Agregar_Cliente();
+void Agregar_Auto();
+
+void MenuEliminar();
+void Eliminar_Cliente();
+void Eliminar_Auto();
+
+void MenuMostrar();
+void Mostrar_Clientes();
+void Mostrar_Autos();
+void Mostrar_Cliente_x_id();
+void Mostrar_Auto_x_id();
+
+void MenuModificar();
+void Modificar_Auto();
+void Modificar_Cliente();
+
+void MenuEstadistica();
+void Estadistica_General();
+void Autos_Comprados();
+void Autos_Vendidos();
+void Autos_sin_vender();
+#pragma endregion
