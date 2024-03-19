e5d5a5b6283aa7694e6fe2be00583d651f78386a
SofiVAM578 <147646915+SofiVAM578@users.noreply.github.com>
2024-03-19 19:04:42 -0400

    feat: implementacion de las funciones eliminar autos/veiculos

--- src/main.cpp
+++ src/main.cpp
@@ -180,4 +180,131 @@ void Agregar_Auto() {
 	cars_write.close();
 }
 #pragma endregion
+
+#pragma region Eliminar
+void MenuEliminar() {
+	int opcion1;
+	cout << "_______________ \n " << endl;
+	cout << "||||     |||| O P C I O N   D E ||||     ||||" << endl;
+	cout << "||||   |||| E L I M I N A C I O N ||||   ||||" << endl;
+	cout << "_______________ \n" << endl;
+
+	cout << " 1- Eliminar Cliente" << endl; 
+	cout << " 2- Eliminar Vendedor" << endl; 
+	cout << " 3- Eliminar Vehiculo" << endl; 
+	cout << " 4- Menu Principal" << endl; 
+	cout << " Opcion: "; cin >> opcion1;
+
+	switch (opcion1) {
+	case 1: Eliminar_Cliente(); MenuEliminar(); break;
+	case 2: Eliminar_Cliente(); MenuEliminar(); break;
+	case 3: Eliminar_Auto(); MenuEliminar(); break;
+	case 4: MenuPrincipal(); break;
+	case 0: break;
+	default: cout << "Opcion incorrecta" << endl << endl;
+	}
+}
+void Eliminar_Cliente() {
+	ifstream clients_read("..\\assets\\clients.csv");
+	ofstream clients_write("..\\assets\\clients.csv", ios::app);
+	ofstream clients_Temp("..\\assets\\clients_temp.csv", ios::in);
+	string line, id_eliminar;
+	bool existe = false;
+	cout << "Ingrese el id del cliente que desea eliminar: "; cin >> id_eliminar;
+	clients_Temp << Clientes << endl;
+	getline(clients_read, line);
+	while (getline(clients_read, line)) {
+		istringstream ss(line);
+		Client Clients;
+		string id_str, age_str;
+		getline(ss, id_str, ';');
+		Clients.id = stoi(id_str);
+		getline(ss, Clients.first_name, ';');
+		getline(ss, Clients.last_name, ';');
+		getline(ss, Clients.email, ';');
+		getline(ss, age_str, ';');
+		Clients.age = stoi(age_str);
+
+		if (id_eliminar.compare(id_str) == 0) existe = true;
+		else clients_Temp << Clients.id << ";" << Clients.first_name << ";" << Clients.last_name << ";" << Clients.email << ";" << Clients.age << endl;
+	}
+
+	clients_read.close();
+	clients_Temp.close();
+	clients_write.close();
+
+	if (existe) {
+		remove("..\\assets\\clients.csv");
+		cout << "El cliente fue eliminado correctamente" << endl;
+		int NowClient = rename("..\\assets\\clients_temp.csv", "..\\assets\\clients.csv");
+		if (NowClient == 0) {
+			ofstream Nuevo("..\\assets\\clients_temp.csv");
+		}
+		else {
+			cout << "Hubo un fallo en la insercion de los registros" << endl;
+		}
+
+	}
+	else {
+		cout << "Cliente con el ID: " << id_eliminar << " no existe en los registros" << endl;
+	}
+}
+void Eliminar_Auto() {
+
+	ifstream cars_read("..\\assets\\cars_data.csv");
+	ofstream cars_write("..\\assets\\cars_data.csv", ios::app);
+	ofstream cars_Temp("..\\assets\\cars_data_temp.csv", ios::in);
+	string line, id_eliminar;
+	bool existe = false;
+	cout << "Ingrese el id del automovil que desea eliminar: "; cin >> id_eliminar;
+	cars_Temp << Autos << endl;
+	getline(cars_read, line);
+	while (getline(cars_read, line)) {
+		istringstream ss(line);
+		Car Cars;
+		string id_str, year_str, sold_to_str, sold_for_str, bought_to_str, bought_for_str;
+
+		getline(ss, id_str, ';');
+		Cars.id = stoi(id_str);
+		getline(ss, Cars.maker, ';');
+		getline(ss, Cars.model, ';');
+		getline(ss, year_str, ';');
+		Cars.year = stoi(year_str);
+		getline(ss, sold_to_str, ';');
+		Cars.sold_to = stoi(sold_to_str);
+		getline(ss, bought_to_str, ';');
+		Cars.bought_to = stoi(bought_to_str);
+		getline(ss, sold_for_str, ';');
+		Cars.sold_for = stoi(sold_for_str);
+		getline(ss, bought_for_str, ';');
+		Cars.bought_for = stoi(bought_for_str);
+
+		if (id_eliminar.compare(id_str) == 0) existe = true;
+		else cars_Temp << Cars.id << ";" << Cars.maker << ";" << Cars.model << ";" << Cars.year << ";" << Cars.sold_to << ";" << Cars.bought_to << ";" << Cars.sold_for << ";" << Cars.bought_for << endl;
+	}
+
+	cars_read.close();
+	cars_Temp.close();
+	cars_write.close();
+
+	if (existe) {
+		remove("..\\assets\\cars_data.csv");
+		cout << "El automovil fue eliminado correctamente" << endl;
+		int NowCar = rename("..\\assets\\cars_data_temp.csv", "..\\assets\\cars_data.csv");
+		if (NowCar == 0) {
+			ofstream Nuevo("..\\assets\\cars_data_temp.csv");
+		}
+		else {
+			cout << "Hubo un fallo en la insercion de los registros" << endl;
+		}
+
+	}
+	else {
+		cout << "Auto con el ID: " << id_eliminar << " no existe en los registros" << endl;
+	}
+}
 #pragma endregion
+
+int main() {
+    MenuPrincipal();
+}
