979fcce9342dd509a6eb1dc223f03d84decf5ab5
luiscab19 <luis19alvarez05@gmail.com>
2024-04-08 22:27:01 -0400

    feat: implementacion de los prototipos de las funciones realcionadas al auto en model.h

--- src/modelo/model.h
+++ src/modelo/model.h
@@ -26,6 +26,37 @@ public:
     }
 };
 
+class AutoController {
+private:
+	AutoModel model;
+	AutoView view;
+public:
+	void AgregarAuto(string marca, string modelo, string año, string comprado) {
+		model.agregar_a_csv("../assets/cars_data.csv", marca, modelo, año, comprado);
+	}
+	void EliminarAuto(string id) {
+		model.eliminar_de_csv("../assets/cars_data.csv", id);
+	}
+	void ModificarAuto(string id_modificar) {
+		model.modificar_csv("../assets/cars_data.csv" , id_modificar);
+	}
+	void MostrarAuto() {
+		view.mostrar_registros_csv("../assets/cars_data.csv");
+	}
+	void MostrarAutoxID(string id){
+        view.mostrar_x_id_csv("../assets/cars_data.csv", id);
+	}
+	void Stockautos(){
+		view.stock("../assets/cars_data.csv");
+	}
+	void stockvendido(){
+		view.vendido("../assets/cars_data.csv");
+	}
+	void stocksinvender(){
+		view.sinverder("../assets/cars_data.csv");
+	}
+};
+
 class Estadisticacontroller {
 private:
 	EstadisticaView view;
